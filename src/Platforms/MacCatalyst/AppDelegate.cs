11696a59f9a4b907a26e29c30e138ab6be456294
Ronan Burke <burkuscat@gmail.com>
2024-04-06 13:31:35 +0100

    New MAUI project

---
+++ src/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NextPage
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
