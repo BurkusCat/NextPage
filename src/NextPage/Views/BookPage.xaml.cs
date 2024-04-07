ee6d4d3324abc8c79526ebd2d1fc02c4e68f24ed
Ronan Burke <burkuscat@gmail.com>
2024-04-07 01:03:23 +0100

    Handle back button presses

--- src/NextPage/Views/BookPage.xaml.cs
+++ src/NextPage/Views/BookPage.xaml.cs
@@ -1,9 +1,40 @@
-﻿namespace NextPage.Views;
+﻿using NextPage.ViewModels;
 
+namespace NextPage.Views;
+
+[DisableBackButtonNavigator]
 public partial class BookPage : ContentPage
 {
+    private BookPageViewModel viewModel;
+
     public BookPage()
     {
+        viewModel = BindingContext as BookPageViewModel;
+
         InitializeComponent();
+    }
+
+    protected override void OnBindingContextChanged()
+    {
+        base.OnBindingContextChanged();
+
+        if (viewModel == null)
+        {
+            viewModel = (BookPageViewModel)BindingContext;
+        }
+    }
+
+    protected override bool OnBackButtonPressed()
+    {
+        if (!viewModel.IsEditing)
+        {
+            // allow users to freely leave when not editing
+            return BackButtonNavigator.HandleBackButtonPressed();
+        }
+
+        _ = viewModel.CancelCommand.ExecuteAsync(null);
+
+        // cancel the back button command
+        return true;
     }
 }
